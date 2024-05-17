04f885ec5f056f6f37355e8dfd8bfc9088131fd6
wndudtjd <dudtjdwn1575@naver.com>
2024-05-17 17:09:29 +0900

    정수 삼각형

---
+++ Programmers/lv.3/240517.정수 삼각형/정수 삼각형.py
@@ -0,0 +1,12 @@
+def solution(triangle):
+    answer = 0
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:	# 왼쪽 끝이면
+                triangle[i][j] += triangle[i-1][0]  # 이전 층의 0번째 값 더하기
+            elif j == len(triangle[i])-1:	# 오른쪽 끝이면
+                triangle[i][j] += triangle[i-1][-1]	# 이전 층의 -1번째 값 더하기
+            else:
+                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+
+    return max(triangle[-1])
