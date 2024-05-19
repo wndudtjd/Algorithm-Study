b8d0c8e13f552f0101ef29518c5096bee5e4baa5
wndudtjd <dudtjdwn1575@naver.com>
2024-05-19 20:54:48 +0900

    n개의 최소공배수

---
+++ Programmers/lv.2/240519.n개의 최소공배수/n개의 최소공배수.py
@@ -0,0 +1,13 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def solution(arr):
+    answer = arr[0]
+    for i in range(1, len(arr)):
+        answer = lcm(answer, arr[i])
+    return answer
